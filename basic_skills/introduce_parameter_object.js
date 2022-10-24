d974ed7edb6d118cbad4ddd5709dbf348a5348bc
kmlee78 <jaoming787@gmail.com>
2022-10-24 23:14:53 +0900

    introduce parameter object

---
+++ basic_skills/introduce_parameter_object.js
@@ -0,0 +1,39 @@
+const station = {
+    name: "ZB1",
+    readings: [
+        { temp: 47, time: "2016-11-10 09:10" },
+        { temp: 53, time: "2016-11-10 09:20" },
+        { temp: 58, time: "2016-11-10 09:30" },
+        { temp: 53, time: "2016-11-10 09:40" },
+        { temp: 51, time: "2016-11-10 09:50" },
+    ]
+};
+
+function readingOutsideRange(station, min, max) {
+    return station.readings.filter(r => r.temp < min || r.temp > max);
+}
+
+alerts = readingOutsideRange(station,
+    operatingPlan.temperatureFloor,
+    operatingPlan.temperatureCeiling
+);
+
+
+/*------------------------refactoring-------------------------*/
+
+
+class NumberRange {
+    constructor(min, max) {
+        this._data = { min: min, max: max };
+    }
+    get min() { return this._data.min; }
+    get max() { return this._data.max; }
+}
+
+function readingOutsideRange(station, range) {
+    return station.readings
+        .filter(r => r.temp < range.min || r.temp > range.max);
+};
+
+const range = new NumberRange(operatingPlan.temperatureFloor, operatingPlan.temperatureCeiling);
+alerts = readingOutsideRange(station, range);
