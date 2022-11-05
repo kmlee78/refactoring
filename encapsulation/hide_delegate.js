ac85adf78bd3b74b5324a8eadaf2ac913f810f17
kmlee78 <jaoming787@gmail.com>
2022-11-06 01:10:28 +0900

    hide delegate

---
+++ encapsulation/hide_delegate.js
@@ -0,0 +1,31 @@
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() { return this._name; }
+    get department() { return this._department; }
+    set department(arg) { this._department = arg; }
+}
+
+class Department {
+    get chargeCode() { return this._chargeCode; }
+    set chargeCode(arg) { this._chargeCode = arg; }
+    get manager() { return this._manager; }
+    set manager(arg) { this._manager = arg; }
+}
+
+manager = aPerson.department.manager;
+
+
+/*------------------------refactoring-------------------------*/
+
+
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() { return this._name; }
+    get manager() { return this._department.manager; }
+}
+
+manager = aPerson.manager;
