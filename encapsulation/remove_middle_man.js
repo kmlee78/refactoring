dcf310dbf2feec696f72984a70652bd179202973
kmlee78 <jaoming787@gmail.com>
2022-11-06 01:24:30 +0900

    remove middle man

---
+++ encapsulation/remove_middle_man.js
@@ -0,0 +1,30 @@
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() { return this._name; }
+    get manager() { return this._department.manager; }
+}
+
+class Department {
+    get chargeCode() { return this._chargeCode; }
+    set chargeCode(arg) { this._chargeCode = arg; }
+    get manager() { return this._manager; }
+    set manager(arg) { this._manager = arg; }
+}
+
+manager = aPerson.manager;
+
+
+/*------------------------refactoring-------------------------*/
+
+
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() { return this._name; }
+    get department() { return this._department; }
+}
+
+manager = aPerson.department.manager;
