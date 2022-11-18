8eb2998ff4fe19f91a8fef6d8a4c5bf99a34dc8c
kmlee78 <jaoming787@gmail.com>
2022-11-18 23:42:05 +0900

    rename field

---
+++ data_organization/rename_field.js
@@ -0,0 +1,18 @@
+const organization = { name: "Acme Gooseberries", country: "GB" };
+
+
+/*------------------------refactoring-------------------------*/
+
+
+class Organization {
+    constructor(data) {
+        this._title = data.title;
+        this._country = data.country;
+    }
+    get title() { return this._title; }
+    set title(aString) { this._title = aString; }
+    get country() { return this._country; }
+    set country(aCountryCode) { this._country = aCountryCode; }
+}
+
+const organization = new Organization({ title: "Acme Gooseberries", country: "GB" });
