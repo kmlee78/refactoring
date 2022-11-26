ff51d0f75f4e8e67156b4bdc9f2a29c268ce3bdd
kmlee78 <jaoming787@gmail.com>
2022-11-26 23:44:00 +0900

    decompose conditional

---
+++ simplify_conditional_logic/decompose_conditional.js
@@ -0,0 +1,23 @@
+if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+    charge = quantity * plan.summerRate;
+else
+    charge = quantity * plan.regularRate + plan.regularServiceCharge;
+
+
+/*------------------------refactoring-------------------------*/
+
+
+if (summer())
+    charge = summerCharge();
+else
+    charge = regularCharge();
+
+function summer() {
+    return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+function summerCharge() {
+    return quantity * plan.summerRate;
+}
+function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+}
