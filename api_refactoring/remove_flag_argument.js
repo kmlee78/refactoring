db054ec7a094eaaf672afa1b7c09de9088c40530
kmlee78 <jaoming787@gmail.com>
2022-12-03 14:41:01 +0900

    remove flag argument

---
+++ api_refactoring/remove_flag_argument.js
@@ -0,0 +1,40 @@
+function deliveryDate(anOrder, isRush) {
+    if (isRush) {
+        let deliveryTime;
+        if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+        else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+        else deliveryTime = 3;
+        return anOrder.placedOn.plusDays(1 + deliveryTime);
+    } else {
+        let deliveryTime;
+        if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+        else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+        else deliveryTime = 4;
+        return anOrder.placedOn.plusDays(2 + deliveryTime);
+    }
+}
+
+aShipment.deliveryDate = deliveryDate(anOrder, true);
+aShipment.deliveryDate = deliveryDate(anOrder, false);
+
+
+/*------------------------refactoring-------------------------*/
+
+
+function rushDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else deliveryTime = 3;
+    return anOrder.placedOn.plusDays(1 + deliveryTime);
+}
+function regularDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+    else deliveryTime = 4;
+    return anOrder.placedOn.plusDays(2 + deliveryTime);
+}
+
+aShipment.deliveryDate = rushDeliveryDate(anOrder);
+aShipment.deliveryDate = regularDeliveryDate(anOrder);
